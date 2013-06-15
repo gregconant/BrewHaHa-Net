89ecc7d97feb982e74e7c6b9e861e2b5df19ee15
Greg Conant <greg.conant@gmail.com>
2013-06-15 10:32:48 -0400

    gc - initial commit of project from template

---
+++ BrewHaHaNet/App_Start/FilterConfig.cs
@@ -0,0 +1,10 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace BrewHaHaNet {
+  public class FilterConfig {
+    public static void RegisterGlobalFilters(GlobalFilterCollection filters) {
+      filters.Add(new HandleErrorAttribute());
+    }
+  }
+}
